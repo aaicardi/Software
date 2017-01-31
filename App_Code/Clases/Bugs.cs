8be1cd05fbd823374de6db4768ec09e1657da372
aaicardi <alexaycardi@gmail.com>
2017-01-31 14:51:13 -0500

    Clases de proyecto
    Clases

---
+++ App_Code/Clases/Bugs.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Bugs
+/// </summary>
+public class Bugs
+{
+    public string Mensaje { get; set; }
+    public bool Error { get; set; }
+    public object Resultado { get; set; }
+	public Bugs()
+	{
+		//
+		// TODO: Agregar aquí la lógica del constructor
+		//
+	}
+}
---
+++ App_Code/Clases/Usuario.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Usuario
+/// </summary>
+public class Usuario
+{
+    public string IdUsuario { get; set; }
+    public string Nombre { get; set; }
+    public string Clave { get; set; }
+    public string Email { get; set; }
+    public string PreguntaSeguridad { get; set; }
+    public string RespuestaSeguridad { get; set; }
+    public Boolean Aprobado { get; set; }
+    public Boolean Bloqueado { get; set; }
+    public DateTime FechaCreacion { get; set; }
+    public DateTime UltimaIngreso { get; set; }
+    public string IdToken { get; set; }
+    public int IdUsuarioOperacion { get; set; }
+    public DateTime FechaOperacion { get; set; }
+
+	public Usuario()
+	{
+		//
+		// TODO: Agregar aquí la lógica del constructor
+		//
+	}
+}
