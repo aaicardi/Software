88193b68f56e8f23e793874af43c2456bdfa23f1
aaicardi <alexaycardi@gmail.com>
2017-02-01 09:41:31 -0500

    Clases

---
+++ App_Code/Clases/Pagos.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Pagos
+/// </summary>
+public class Pagos
+{
+
+    public int IdPagos { get; set; }
+    public int IdCliente { get; set; }
+    public int IdPrestamo { get; set; }
+    public DateTime FechaPago { get; set; }
+    public decimal Monto_Pagado { get; set; }
+    public decimal Resta_Deuda { get; set; }
+    public int Cuota_Numero { get; set; }
+	public Pagos()
+	{
+		//
+		// TODO: Agregar aquí la lógica del constructor
+		//
+	}
+}
---
+++ App_Code/Clases/Prestamo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+/// <summary>
+/// Descripción breve de Prestamo
+/// </summary>
+public class Prestamo
+{
+    public int IdPrestamo { get; set;}
+    public int IdCliente { get; set; }
+    public decimal Monto_Prestado { get; set; }
+    public int Porcentaje_Interes { get; set; }
+    public DateTime Fecha_Prestamo { get; set; }
+    public DateTime Fecha_Max_Pago { get; set; }
+    public int IdTipoPago { get; set; }
+    public int Total_Con_Interes { get; set; }
+    public int Numero_Cuotas { get; set; }
+	public Prestamo()
+	{
+		//
+		// TODO: Agregar aquí la lógica del constructor
+		//
+	}
+}
